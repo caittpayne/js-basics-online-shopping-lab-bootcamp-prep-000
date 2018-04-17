bc5d3fcd3b24a2eb8b3f6d2911b719f4724b1600
Cait Payne <caitpayne@outlook.com>
2018-04-17 23:46:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,11 +52,7 @@ function removeFromCart(item) {
       
       return cart;
     }
-    else {
-      return('That item is not in your cart.')
-    }
   }
-
 }
 
 function placeOrder(cardNumber) {
